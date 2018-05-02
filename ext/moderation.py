5344e4d05038cdcf6b27ba434a6d9d23081af459
davfsa <davfsa@gmail.com>
2018-05-02 18:50:19 +0200

    Update moderation.py

--- ext/moderation.py
+++ ext/moderation.py
@@ -7,14 +7,14 @@ class Moderation:
 	
 	@commands.command(pass_context=True)
 	async def prefix(self, ctx, new_prefix=None):
-		"""Change the bot prefix"""
+		"""Changes the bot prefix"""
 		if ctx.message.author.id != '377812572784820226' and ('manage_server', True) not in ctx.message.author.server_permissions:
 			await self.bot.say("You are not allowed to perform this action, missing `Manage Server`")
 			return	
 
 		old_prefix = self.bot.command_prefix(self.bot, ctx.message)
 		if new_prefix == None:
-			await bot.say("The prefix for this server is \"%s\"" %(old_prefix))		
+			await self.bot.say("The prefix for this server is \"%s\"" %(old_prefix))		
 			return	
 		if old_prefix == new_prefix:
 			await self.bot.say("Prefix hasn't changed")
