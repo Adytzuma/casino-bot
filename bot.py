2acaecee26b21fe42d2d557976bbc725f88af1a6
davfsa <davfsa@gmail.com>
2018-05-03 11:22:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ def command_prefix_generator (bot, message):
 	if message.channel.name == "no-prefix":
 		return ""
 	try:
-		return [prefixes.get(message.server.id, 'c!'), "<@!434057626980974602> "]
+		return prefixes.get(message.server.id, 'c!')
 	except AttributeError:
-		return ["c!", "<@!434057626980974602> "]
+		return "c!"
 
 bot = commands.Bot(command_prefix=command_prefix_generator)
 
