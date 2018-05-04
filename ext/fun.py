039041b73673d0dd64ebc4c462d2e4267c71e139
davfsa <davfsa@gmail.com>
2018-05-04 17:05:01 +0200

    Updated fun.py

--- ext/fun.py
+++ ext/fun.py
@@ -5,15 +5,16 @@ import asyncio
 class Fun:
 	def __init__(self, bot):
 		self.bot = bot
-	@commands.command()
-	async def infect(self, who: discord.Member, what):
-	    async def task():
+
+	@commands.command(pass_contex=True)
+	async def infect(self, ctx, who: discord.Member, what):
+	    async def task(ctx):
 	        while True:
 	            m = await self.bot.wait_for(
 	                'message', 
-	                check=lambda m: m.author.id == ctx.author.id and m.channel.id == ctx.channel.id)
+	                check=lambda m: m.author.id == ctx.author.id and m.server.id == ctx.server.id)
 	            await m.add_reaction(what)
-	    future = self.bot.loop.create_task(task())
+	    future = self.bot.loop.create_task(task(ctx))
 	    await asyncio.sleep(60 * 60)
 	    future.cancel()
 
