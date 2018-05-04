7e0c0cb250b3c00c010ccfac8231fe53dd8a4afb
davfsa <davfsa@gmail.com>
2018-05-04 17:27:48 +0200

    Updated fun.py

--- ext/fun.py
+++ ext/fun.py
@@ -8,11 +8,9 @@ class Fun:
 
 	@commands.command(pass_context=True)
 	async def infect(self, ctx, who: discord.Member, what):
-	    async def task(ctx):
+	    async def task(con):
 	        while True:
-	            m = await self.bot.wait_for(
-	                'message', 
-	                check=lambda m: m.author.id == ctx.author.id and m.server.id == ctx.server.id)
+	            m = await self.bot.wait_for_message(check=lambda m: m.author.id == con.author.id and m.server.id == con.server.id)
 	            await m.add_reaction(what)
 	    future = self.bot.loop.create_task(task(ctx))
 	    await asyncio.sleep(60 * 60)
