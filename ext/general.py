3aecb19974bc143256936387783438c0d63f91d1
davfsa <davfsa@gmail.com>
2018-05-11 09:40:43 +0000

    Update general.py

--- ext/general.py
+++ ext/general.py
@@ -18,6 +18,14 @@ class General:
 		"""Returns the github URL of the bot"""
 		await self.bot.say("%s, the link for the source code of this bot is: https://github.com/davfsa/casino-bot/" %(ctx.message.author.mention))
 
-
+	@commands.command(pass_context=True)
+	async def invite(self, ctx):
+		"""Gives the invite for the official supporter bot server"""
+		await self.bot.say("%s, the link for the official supporter bot server is: https://discord.gg/hrN3hmz/" %(ctx.message.author.mention))
+	
+	@commands.command(pass_context=True)
+	async def add(self, ctx):
+		"""Sends the invite to add the bot to your server"""
+		await self.bot.say("%s, the link to invite the bot is: https://discordapp.com/api/oauth2/authorize?client_id=434057626980974602&permissions=330752&scope=bot" 
 def setup(bot):
 	bot.add_cog(General(bot))
