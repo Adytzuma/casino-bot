1f97e295d46dbd33e820724cd89a8cf70817365a
davfsa <davfsa@gmail.com>
2018-05-16 09:45:53 +0000

    Update poker.py

--- ext/poker.py
+++ ext/poker.py
@@ -12,7 +12,7 @@ class Poker:
 		self.card_numbers = {"1" : "Ace", "2" : "Two", "3" : "Three", "4" : "Four", "5" : "Five", "6" : "Six", "7" : "Seven", "8" : "Eight", "9" : "Nine", "10" : "Ten", "11" : "Jack", "12" : "Queen", "13" : "King"}
 		self.card_symbols = {"1" : "Spades", "2" : "Clubs", "3" : "Hearts", "4" : "Diamonds"}
 		self.actions = ["", "", "", "", ""]
-		self.msgs = {"choose1" : "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n::\tTrash a card\n:x:\tGive up", "choose2", "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n:x:\tGive up"}
+		self.msgs = {"choose1" : "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n::\tTrash a card\n:x:\tGive up", "choose2" : "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n:x:\tGive up"}
 		
 	#Waiting Games Hierarchy
 	"""
