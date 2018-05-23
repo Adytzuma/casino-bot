037d8d5614045b8da501d5957e4036d8ca7a1b57
davfsa <davfsa@gmail.com>
2018-05-23 19:54:34 +0200

    Add files via upload

--- ext/general.py
+++ ext/general.py
@@ -2,40 +2,57 @@ import discord
 from discord.ext import commands
 import asyncio
 
-class General:
-	def __init__ (self, bot):
-		self.bot = bot
-		
-	@commands.command(pass_context=True)
-	async def ping(self, ctx):
-		"""Returns Pong"""
-		t = await self.bot.say('Pong!')
-		ms = (t.timestamp-ctx.message.timestamp).total_seconds() * 1000
-		await self.bot.edit_message(t, new_content='Ping time: {}ms'.format(int(ms)))
-		
-	@commands.command(pass_context=True)
-	async def github(self, ctx):
-		"""Returns the github URL of the bot"""
-		await self.bot.say("%s, the link for the source code of this bot is: https://github.com/davfsa/casino-bot/" %(ctx.message.author.mention))
 
-	@commands.command(pass_context=True)
-	async def invite(self, ctx):
-		"""Gives the invite for the official supporter bot server"""
-		await self.bot.say("%s, the link for the official supporter bot server is: https://discord.gg/aDtBVA4/" %(ctx.message.author.mention))
-	
-	@commands.command(pass_context=True)
-	async def add(self, ctx):
-		"""Sends the invite to add the bot to your server"""
-		await self.bot.say("%s, the link to invite the bot is: https://discordapp.com/api/oauth2/authorize?client_id=434057626980974602&permissions=330752&scope=bot" %(ctx.message.author.mention))
+class General():
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command(pass_context=True)
-	async def feedback(self, ctx, *, message=None):
-		"""Sends feedback to the bot developers"""
-		if message is None:
-			await self.bot.say("Please provide a message. Do `c!help feedback` for more info")	
-			return
+    @commands.command()
+    async def ping(self, ctx):
+        'Returns Pong'
+        t = await ctx.send('Pong!')
+        ms = (t.created_at - ctx.message.created_at).total_seconds() * 1000
+        await t.edit(new_content='Ping time: {}ms'.format(int(ms)), content='Ping time: {}ms'.format(int(ms)))
 
-		await self.bot.send_message(self.bot.get_channel("446296161528315904"), f"**Feedback:**\nUser: {ctx.message.author.mention}\nContent: {message}")
-		await self.bot.say("Your feedback has been sent to the team:thumbsup:")	
+    @commands.command()
+    async def github(self, ctx):
+        'Returns the github URL of the bot'
+        await ctx.send('%s, the link for the source code of this bot is: https://github.com/davfsa/casino-bot/' %
+                       ctx.author.mention)
+
+    @commands.command()
+    async def invite(self, ctx):
+        'Gives the invite for the official supporter bot server'
+        await ctx.send(
+            '%s, the link for the official supporter bot server is: https://discord.gg/aDtBVA4/' % ctx.author.mention)
+
+    @commands.command()
+    async def add(self, ctx):
+        'Sends the invite to add the bot to your server'
+        await ctx.send(
+            '%s, the link to invite the bot is: https://discordapp.com/api/oauth2/authorize?client_id=434057626980974602&permissions=330752&scope=bot'
+            % ctx.author.mention)
+
+    @commands.command()
+    async def feedback(self, ctx, *, message=None):
+        'Sends feedback to the bot developers'
+        if message is None:
+            await ctx.send('Please provide a message. Do `c!help feedback` for more info')
+            return
+        await self.bot.get_channel(446296161528315904).send((
+            ('**Feedback:**\nUser: ' + ctx.author.mention) + '\nContent: ') + message)
+        await ctx.send('Your feedback has been sent to the team :thumbsup:')
+
+    @commands.command()
+    async def bug(self, ctx, *, message=None):
+        'Sends a bug report to the bot developers'
+        if message is None:
+            await ctx.send('Please provide a message. Do `c!help bug` for more info')
+            return
+        await self.bot.get_channel(446296161528315904).send((
+            ('**Bug report:**\nUser: ' + ctx.author.mention) + '\nBug: ') + message)
+        await ctx.send('Your bug report has been sent to the team :thumbsup:')
+
+
 def setup(bot):
-	bot.add_cog(General(bot))
+    bot.add_cog(General(bot))
--- ext/poker.py
+++ ext/poker.py
@@ -1,176 +1,194 @@
 import discord
 from discord.ext import commands
 from random import randint
+dev_channels = [416161836069814273, 426699644190326785, 441474231176396800]
 
-dev_channels = ["416161836069814273", "426699644190326785", "441474231176396800"]
 
-class Poker:
-	def __init__ (self, bot):
-		self.bot = bot
-		self.waiting_games = {}
-		self.running_games = []
-		self.card_numbers = {"1" : "Ace", "2" : "Two", "3" : "Three", "4" : "Four", "5" : "Five", "6" : "Six", "7" : "Seven", "8" : "Eight", "9" : "Nine", "10" : "Ten", "11" : "Jack", "12" : "Queen", "13" : "King"}
-		self.card_symbols = {"1" : "Spades", "2" : "Clubs", "3" : "Hearts", "4" : "Diamonds"}
-		self.actions = ["", "", "", "", ""]
-		self.msgs = {"choose1" : "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n::\tTrash a card\n:x:\tGive up", "choose2" : "**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n:x:\tGive up"}
-		
-	#Waiting Games Hierarchy
-	"""
-	{Channel id:[Player1, Player2, Player3, Player4]}
-	"""
-	
-	#Running Games Hierarchy
-	"""
-	[Channel id]
-	"""
-	
-	def is_testing_channel(ctx):
-		if ctx.message.server.id == "415241422736719882" and ctx.message.channel.id in dev_channels:
-			return True
-		return False
-	
-	def get_card(self, num):
-		#Num1 = Symbol
-		#Num2 = Number
+class Poker():
+    def __init__(self, bot):
+        self.bot = bot
+        self.waiting_games = {}
+        self.running_games = []
+        self.card_numbers = {
+            '1': 'Ace',
+            '2': 'Two',
+            '3': 'Three',
+            '4': 'Four',
+            '5': 'Five',
+            '6': 'Six',
+            '7': 'Seven',
+            '8': 'Eight',
+            '9': 'Nine',
+            '10': 'Ten',
+            '11': 'Jack',
+            '12': 'Queen',
+            '13': 'King',
+        }
+        self.card_symbols = {
+            '1': 'Spades',
+            '2': 'Clubs',
+            '3': 'Hearts',
+            '4': 'Diamonds',
+        }
+        self.actions = ['', '', '', '', '']
+        self.msgs = {
+            'choose1': '**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n::\tTrash a card\n:x:\tGive up',
+            'choose2': '**Choose:**\n:spades:\tShows your cards (you will have another action)\n::\tGo in\n::\tRaise bet\n:x:\tGive up',
+        }
+
+    #Waiting Games Hierarchy
+    """{Channel id:[Player1, Player2, Player3, Player4]}"""
+    
+    #Running Games Hierarchy
+    """[Channel id]"""
+
+    def is_testing_channel(ctx):
+        if (ctx.guild.id == 415241422736719882) and (ctx.channel.id in dev_channels):
+            return True
+        return False
+
+    def get_card(self, num):
+		#First number = Symbol
+		#Second number = Number
 		#Eg: 12 = Two of Spades
 		#Eg: 35 = Five of Clubs
 		
-		i = 1
-		nm = ""
-		for c in num:
-			if i == 1:
-				sym = self.card_symbols[c]
-			else:
-				nm = nm + self.card_numbers[c]
-			i += 1
+        i = 1
+        nm = ''
+        for c in num:
+            if i == 1:
+                sym = self.card_symbols[c]
+            else:
+                nm = nm + self.card_numbers[c]
+            i += 1
+        return (nm + ' of ') + sym
 
-		return nm + " of " + sym
+    def generate_deck():
+        deck = []
+        #Generates a empty deck
+        for i in range(0, 52):
+            deck.append('')
+           
+        #Generates a deck with all posible cards
+        symbols = self.card_symbols
+        numbers = self.card_numbers
+        cards = []
+        for sym in symbols:
+            for num in numbers:
+                cards.append(sym + num)
+                
+        #Adds the cards to the deck in a random order
+        for c in cards:
+            card = cards[randint(0, len(cards))]
+            deck.append(card)
+            cards.remove(card)
 
-	def generate_deck():
-		deck = []
-		#Generates a empty deck
-		for i in range(0, 52):
-			deck.append("")
+    async def alert(self, info, user1=None, user2=None, user3=None, user4=None):
+        try:
+            await user1.send(info)
+            await user2.send(info)
+            await user3.send(info)
+            await user4.send(info)
+        except:
+            pass
 
-		#Generates a deck with all posible cards
-		symbols = self.card_symbols
-		numbers = self.card_numbers
-		cards = []
-		
-		for sym in symbols:
-			for num in numbers:
-				cards.append(sym + num)
-				
-		for c in cards:
-			card = cards[randint(0, len(cards))]		
-			deck.append(card)
-			cards.remove(card)	     
-						     
-	async def alert(self, info, user1 = None, user2 = None, user3 = None, user4 = None):
-		try:
-			await self.bot.send_message(user1, info)
-			await self.bot.send_message(user2, info)
-			await self.bot.send_message(user3, info)
-			await self.bot.send_message(user4, info)
-		except:
-			pass
-
-	async def start(self, channel_id):
-		self.running_games.append(channel_id)
-		users = self.waiting_games[channel_id]
-		money = [5, 100, 100, 100, 100]
-		cards = [self.generate_deck(), [], [], [], []]
-		del self.waiting_games[channel_id]
-
-		#Deal 4 cards per player
-		for u in range(1, 5):
-			for c in range(1, 5):
-				cards[u].append(cards[0][0])
-				cards[0].remove(cards[0][0])
-			
-		
-		for rn in range(1, 5):
-			for t in range(1, 5):
-				if i == 1:
-					await self.alert_users(f"{users[0]}\'s turn. Wait for your turn", users[1], users[2], users[3])
-				elif i == 2:
-					await self.alert_users(f"{users[1]}\'s turn. Wait for your turn", users[0], users[2], users[3])
-				elif i == 3:
-					await self.alert_users(f"{users[2]}\'s turn. Wait for your turn", users[0], users[1], users[3])
-				else:
-					await self.alert_users(f"{users[3]}\'s turn. Wait for your turn", users[0], users[1], users[2])
-			
-				done = False
-				while done != True:
-					if rn != 1:
-						msg = await self.bot.send_message(users[t -1], self.msgs["choose2"])
-					else:
-						msg = await self.bot.send_message(users[t -1], self.msgs["choose1"])
-
-					with self.actions as a:
-						for r in a:
-							if r == a[3] and rn != 1:
-								pass
-							else:
-								await self.bot.add_reaction(msg, r)
-						if rn == 1:
-							rct = await self.bot.wait_for_reaction(message=msg, emoji=a)
-						else:
-							aa = a
-							aa.remove(a[3])
-							rct = await self.bot.wait_for_reaction(message=msg, emoji=aa)
-
-						if rtc == a[0]:
-							#Show cards
-							content = "**Your cards:**\n"
-							for c in cards[i]:
-								card = self.get_card(c)
-								content = content + f"\t{card}\n"
-							await self.bot.delete_message(msg)
-							await self.bot.send_message(users[t -1], content)
-							
-						elif rtc == a[1]:
-							#Go in
-							done = True
-						elif rtc == a[2]:
-							#Raise bet
-							done = True
-						elif rtc == a[3]:
-							#Trash a card
-							done = True
-						elif rtc == a[4]:
-							#Give up
-							done = True
-					
-
-	@commands.check(is_testing_channel)
-	@commands.command(pass_context=True)
-	async def join(self, ctx):
-		"""Join a poker game on a channel"""
-		channel_id = ctx.message.channel.id
-		user= ctx.message.author
-		
-		if channel_id in self.running_games:
-			await self.bot.say('There is already a game running in this channel')
-			return
-		
-		if channel_id in self.waiting_games:
-			if user in self.waiting_games[channel_id]:
-	        	        await self.bot.say('You are already in the game')
-	        	        return
-			
-			self.waiting_games[channel_id].append(user)
-        	
-			if len(self.waiting_games[channel_id]) == 4:
-				await self.bot.say('Game starting')
-				await self.bot.loop.create_task(start(channel_id))
-				return
+    async def start(self, channel_id):
+        self.running_games.append(channel_id)
+        users = self.waiting_games[channel_id]
+        money = [5, 100, 100, 100, 100]
+        cards = [self.generate_deck(), [], [], [], []]
+        del self.waiting_games[channel_id]
         
-			await self.bot.say('<@!%s>, you joined the game, %s players remaining!' %(user.id, 4 - len(self.waiting_games[channel_id])))
-			return
+        #Deal 4 cards per player
+        for u in range(1, 5):
+            for c in range(1, 5):
+                cards[u].append(cards[0][0])
+                cards[0].remove(cards[0][0])
+        
+        for rn in range(1, 5):
+            for t in range(1, 5):
+                if i == 1:
+                    await self.alert_users(users[0].mention + "'s turn. Wait for your turn", users[1], users[2],
+                                           users[3])
+                elif i == 2:
+                    await self.alert_users(users[1].mention + "'s turn. Wait for your turn", users[0], users[2],
+                                           users[3])
+                elif i == 3:
+                    await self.alert_users(users[2].mention + "'s turn. Wait for your turn", users[0], users[1],
+                                           users[3])
+                else:
+                    await self.alert_users(users[3].mention + "'s turn. Wait for your turn", users[0], users[1],
+                                           users[2])
+                done = False
+                while done != True:
+                    if rn != 1:
+                        msg = await users[t - 1].send(self.msgs['choose2'])
+                    else:
+                        msg = await users[t - 1].send(self.msgs['choose1'])
+                    with self.actions as a:
+                        for r in a:
+                            if (r == a[3]) and (rn != 1):
+                                pass
+                            else:
+                                await msg.add_reaction(r)
+                        if rn == 1:
+							
+                            rct = await self.bot.wait_for('reaction_add')
+                        else:
+                            
+                            aa = a
+                            aa.remove(a[3])
+                            rct = await self.bot.wait_for('reaction_add')
+                            
+                        if rtc == a[0]:
+							#Show cards
+                            content = '**Your cards:**\n'
+                            for c in cards[i]:
+                                card = self.get_card(c)
+                                content = content + '\t{}\n'.format(card)
+                            await msg.delete()
+                            await users[t - 1].send(content)
+                        elif rtc == a[1]:
+							#Go in
+                            done = True
+                        elif rtc == a[2]:
+							#Raise bet
+                            done = True
+                        elif rtc == a[3]:
+							#Trash a card
+                            done = True 
+                        elif rtc == a[4]:
+							#Give up
+                            done = True
 
-		self.waiting_games.update({channel_id:[user]})
-		await self.bot.say('<@!%s>, you joined the game, %s players remaining!' %(user.id, 4 - len(self.waiting_games[channel_id])))
-			
+
+    @commands.check(is_testing_channel)
+    @commands.command()
+    async def join(self, ctx):  
+        'Join a poker game on a channel'
+        channel_id = ctx.channel.id
+        user = ctx.author
+        if channel_id in self.running_games:
+            await ctx.send('There is already a game running in this channel')
+            return
+        if channel_id in self.waiting_games:
+            if user in self.waiting_games[channel_id]:
+                await ctx.send('You are already in the game')
+                return
+            self.waiting_games[channel_id].append(user)
+            if len(self.waiting_games[channel_id]) == 4:
+                await ctx.send('Game starting')
+                await self.bot.loop.create_task(start(channel_id))
+                return
+            await ctx.send('%s, you joined the game, %s players remaining!' % (user.mention,
+                                                                               4 - len(self.waiting_games[channel_id])))
+            return
+        self.waiting_games.update({
+            channel_id: [user],
+        })
+        await ctx.send(
+            '%s, you joined the game, %s players remaining!' % (user.mention, 4 - len(self.waiting_games[channel_id])))
+
+
 def setup(bot):
-	bot.add_cog(Poker(bot))
+    bot.add_cog(Poker(bot))
