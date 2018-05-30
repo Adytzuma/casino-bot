9482596c65c653fe3aadaf87a4b14a757f851164
davfsa <davfsa@gmail.com>
2018-05-30 19:09:37 +0200

    Updated bot.py

--- bot.py
+++ bot.py
@@ -58,9 +58,9 @@ async def on_ready():
 	
 @bot.event
 async def on_command_error(ctx, error):
-	if isinstance(error, commands.CommandNotFound):
+	if isinstance(error, commands.CommandNotFound) or isinstance(error, commands.CheckFailure):
 		return
-	await ctx.send("Ups. An unespected error has been raised, the error has been reported to the developers and will be fixed soon :smile:")
+	await ctx.send("Ups. An unexpected error has been raised, the error has been reported to the developers and will be fixed soon :smile:")
 	error = error.__cause__ or error
 	tb = traceback.format_exception(type(error), error, error.__traceback__, limit=2, chain=False)
 	tb = ''.join(tb)
