458bd756b012a447c71801e3d1d47f4e85ef3daf
davfsa <davfsa@gmail.com>
2018-06-27 10:44:07 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,6 @@ async def on_command_error(ctx, error):
 	await ctx.send("Ups. An unexpected error has been raised, the error has been reported to the developers and will be fixed soon :smile:")
 	error = error.__cause__ or error
 	tb = traceback.format_exception(type(error), error, error.__traceback__)
-	tb = ''.join(tb)
 	fmt = '**`Error in command {}`**\n\n**{}:**\n```py\n{}\n```'.format(ctx.command, type(error).__name__, tb)
 	await bot.get_channel(446291887524020224).send(fmt) 
 
