3a0bba61ef3da1731a77b9443cc39c3a47536709
davfsa <davfsa@gmail.com>
2018-06-29 17:00:35 +0000

    Updated admin.py

--- ext/admin.py
+++ ext/admin.py
@@ -1,96 +1,160 @@
-from discord.ext import commands
-import discord
-import contextlib
-import discord
-import traceback
-import io
-import time
-import datetime
-import math
+# -*- coding: utf-8 -*-
+"""
+Exec command for calling stuff in either the current interpreter or shell. This
+also contains the logic to restart the bot.
+
+A panic subcommand is provided to quickly eject this cog. The bot would then
+require an alternative way of restarting and exec'ing arbitrary code until it
+restarts or has the cog reloaded.
+"""
 import asyncio
-import inspect
-import textwrap
-from discomaton.factories import bookbinding
-import async_timeout
+import contextlib
+import io
+import pprint
+import re
+import shutil
+import sys
+import time
+import traceback
 
-# MIT License
-#
-# Copyright (c) 2018 Davfsa
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+import discord
+from discord.ext import commands
 
-global admin_perm_id
-admin_perm_id = [377812572784820226]
+from pag import factory
 
 
-class Admin():
-	def __init__(self, bot):
-		self.bot = bot
+class Superuser:
+    """
+    Owner-only hidden exec command.
+    """
+    async def __local_check(self, ctx):
+        return ctx.bot.owner_id == ctx.author.id
 
-	def is_owner(ctx):
-		if ctx.author.id in admin_perm_id:
-			return True
-		return False
-	
-	def cleanup_code(self, content):
-		'Automatically removes code blocks from the code.'
-		if content.startswith('```') and content.endswith('```'):  # remove ```py\n```
-			return '\n'.join(content.split('\n')[1:(-1)])
-		return content 
+    @commands.group(name='sudo', invoke_without_command=True)
+    async def sudo_group(self, ctx):
+        pass
 
-	@commands.check(is_owner)
-	@commands.command() 
-	async def exec(self, ctx, *, command):
-		'Execute or evaluate code in python'
-		binder = bookbinding.StringBookBinder(ctx, max_lines=50,prefix='```py', suffix='```')
-		command = self.cleanup_code(command)
-		
-		try:
-			binder.add_line('# Output:')
-			if command.count('\n') == 0:
-				with async_timeout.timeout(10):
-					if command.startswith('await '):
-						command = command[6:]
-					result = eval(command)
-					if inspect.isawaitable(result):
-						binder.add_line(
-							f'# automatically awaiting result {result}')
-						result = await result
-					binder.add(str(result))
-			else:
-				with async_timeout.timeout(60):
-					with io.StringIO() as output_stream:
-						with contextlib.redirect_stdout(output_stream):
-							with contextlib.redirect_stderr(output_stream):
-								wrapped_command = (
-										'async def _aexec(ctx):\n' +
-										'\n'.join(f'	{line}'
-												  for line in command.split('\n')) +
-										'\n')
-								exec(wrapped_command)
-								result = await (locals()['_aexec'](ctx))
-						binder.add(output_stream.getvalue())
-						binder.add('# Returned ' + str(result))
-		except:
-			binder.add(traceback.format_exc())
-		finally:
-			binder.start()
-			
+    @sudo_group.command(name='exec', aliases=['shell'])
+    async def execute(self, ctx, *, code):
+        """Executes the given code."""
+
+        # Removes code blocks if they are present. This then captures the
+        # syntax highlighting to use if it is present.
+        code_block = re.findall(r"```([a-zA-Z0-9]*)\s([\s\S(^\\`{3})]*?)\s*```", code)
+
+        if code_block:
+            lang, code = code_block[0][0], code_block[0][1]
+
+            if lang == 'py':
+                lang = 'python'
+        else:
+            lang = 'python' if ctx.invoked_with == 'exec' else 'bash'
+
+        executor = self.execute_in_session if lang is 'python' else self.execute_in_shell
+
+        async with ctx.typing():
+            sout, serr, result, exec_time, prog = await executor(ctx, lang, code)
+
+        # Todo: use my pag.
+        pag = factory.StringNavigatorFactory(
+            prefix='```markdown\n',
+            suffix='```',
+            max_lines=25,
+            enable_truncation=False
+        )
+
+        nl = '\n'
+        pag.add_line(f'# Using {prog.replace(nl, " ")}')
+
+        if sout:
+            pag.add_line('# stdout:')
+            for line in sout.split('\n'):
+                pag.add_line(line)
+        if serr:
+            pag.add_line('# stderr:')
+            for line in serr.split('\n'):
+                pag.add_line(line)
+        if len(str(result)) > 100:
+            pag.add_line(f'# Took approx {1000 * exec_time:,.2f}ms; returned:')
+            for p in pprint.pformat(result, indent=4).split('\n'):
+                pag.add_line(p)
+        else:
+            pag.add_line(f'# Returned {result} in approx {1000 * exec_time:,.2f}ms.')
+
+        await pag.start(ctx)
+
+    async def execute_in_session(self, ctx, _program, code):
+        """Executes the code in the current interpreter session."""
+        sout = io.StringIO()
+        serr = io.StringIO()
+
+        # Intrinsics to eval the line where possible if it is one line.
+        # This will implicitly cause the result of await expressions to be
+        # awaited, which is cool.
+        if code.count('\n') == 0:
+            brackets = ('(', ')'), ('[', ']'), ('{', '}')
+
+            def count_check(c1, c2):
+                return code.count(c1) == code.count(c2)
+
+            # If matched brackets
+            if all(count_check(c1, c2) for c1, c2 in brackets):
+                stripped = code.lstrip()
+                if not any(stripped.startswith(x) for x in ('raise', 'return ', 'yield ')):
+                    code = 'return ' + stripped
+
+        # Redirect all streams.
+        with contextlib.redirect_stdout(sout):
+            with contextlib.redirect_stderr(serr):
+                nl = '\n'
+                func = (
+                    'async def aexec(ctx, bot, redis):\n'
+                    f'{nl.join(" " * 4 + line for line in code.split(nl))}'
+                )
+
+                start_time = time.monotonic()
+                # noinspection PyBroadException
+                try:
+                    exec(func)
+                    result = await locals()['aexec'](ctx, ctx.bot, ctx.bot.db_pool)
+                except BaseException as ex:
+                    traceback.print_exc()
+                    result = type(ex)
+                finally:
+                    exec_time = time.monotonic() - start_time
+
+        return sout.getvalue(), serr.getvalue(), result, exec_time, sys.version
+
+    async def execute_in_shell(self, _ctx, program, code):
+        """Executes the given program in-shell."""
+        path = shutil.which(program)
+        if not path:
+            return '', f'{program} not found.', 127, 0.0
+
+        start_time = time.monotonic()
+        process = await asyncio.create_subprocess_exec(
+            path, '--',
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            stdin=asyncio.subprocess.PIPE
+        )
+
+        sout, serr = await process.communicate(bytes(code, 'utf-8'))
+        exec_time = time.monotonic() - start_time
+
+        exit_code = process.returncode
+
+        sout = sout.decode()
+        serr = serr.decode()
+
+        return sout, serr, str(exit_code), exec_time, path
+
+    @sudo_group.command()
+    async def logout(self, ctx):
+        """Makes the bot log out."""
+		await ctx.message.add_reaction()
+        await ctx.bot.logout()
+
 def setup(bot):
-	bot.add_cog(Admin(bot))
+    """Add the cog to the bot."""
+    bot.add_cog(Superuser())
