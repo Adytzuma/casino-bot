59bba6efa6ff2f56a3da96fed5572cbcdd5c652b
davfsa <davfsa@gmail.com>
2018-06-29 18:16:14 +0000

    Update button.py

--- pag/button.py
+++ pag/button.py
@@ -313,6 +313,9 @@ async def lock_unlock(_btn: Button, _react: discord.Reaction, _user: discord.Use
                                                    or m.nick == m_content
                                                    or (m_content.isdigit() and int(m_content) == m.id),
                                          root.guild.members)
+                    if not member:
+                        raise BadArgument
+                        
                     if member.bot:
                         await _btn.owner.send('Cannot transfer to a bot.',
                                               delete_after=10, add_to_list=False)
